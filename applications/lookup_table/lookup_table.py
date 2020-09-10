65f534528003b6fad5b9d9f6587a254bbbe1b662
mikepadiernos <mike.padiernos@gmail.com>
2020-09-10 12:13:36 -0400

    [FEATURE] completion / passed
    - completed code for lookup_table.py

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,4 @@
-# Your code here
+import random, math
 
 
 def slowfun_too_slow(x, y):
@@ -9,13 +9,20 @@ def slowfun_too_slow(x, y):
 
     return v
 
+
+cache = {}
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
 
+    if (x, y) not in cache:
+        v = slowfun_too_slow(x, y)
+        cache[(x, y)] = v
+    return cache[(x, y)]
 
 
 # Do not modify below this line!
